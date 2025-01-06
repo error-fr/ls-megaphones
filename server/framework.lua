5b984a17d5d1c5d05fef1455a762bf6f0f6a666c
pen <61853640+penathon@users.noreply.github.com>
2025-01-06 00:46:24 +0000

    fix: typo for qb

--- server/framework.lua
+++ server/framework.lua
@@ -1,7 +1,7 @@
 if Config.framework == 'qb' then
     QBCore = exports['qb-core']:GetCoreObject()
 
-    QBCore.Functions.CreateUsableItem('megaphone', function(source)
+    QBCore.Functions.CreateUseableItem('megaphone', function(source)
         local Player = QBCore.Functions.GetPlayer(source)
 
         if Player then
