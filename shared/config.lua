0eeba11151e9e82793ef6fd637db48f97ef5370b
penathon <61853640+penathon@users.noreply.github.com>
2025-08-27 13:32:26 +0100

    feat(shared\config): add support for specifying vehicle class

--- shared/config.lua
+++ shared/config.lua
@@ -8,9 +8,10 @@ Config = {
         `v_club_roc_micstd`, 
         `prop_table_mic_01` 
     },
-    volume = -1.0,                  -- A value between 0.0 and 1.0. -1 for the default volume.
-    specifyVehicles = false,        -- If false it will default to all vehicles under the emergency class
-    vehicles = { "washington" },    -- If the above is true, these will be the only vehicles which have a vehicle megaphone.
+    volume = -1.0,                  -- A value between 0.0 and 1.0. -1.0 for the default volume.
+    specifyVehicles = false,        -- If false it will use the vehicleClass value below
+    vehicleClass = {18, 15},        -- If above is set to false it will only apply to these vehicle class
+    vehicles = { "washington" },    -- If specifyVehicles is true, these will be the only vehicles which have a vehicle megaphone.
     framework = 'none',             -- none / qb / qbox / esx
     keybind = 'F12',                -- Default keybind for vehicle megaphone
     debug = false,                  -- Enables printing of basic debug info / polys
